a6f39e2e6673936dda94a179791330420ade7bab
edu-magno <eduardo.magno.u@gmail.com>
2023-12-11 10:15:45 -0300

    auau

--- lib/widgets/footer_widget.dart
+++ lib/widgets/footer_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
 
 class FooterWidget extends StatelessWidget {
   const FooterWidget({super.key});
@@ -11,27 +10,26 @@ class FooterWidget extends StatelessWidget {
         color: const Color(0xff5A2684),
         width: MediaQuery.sizeOf(context).width * 1,
         height: MediaQuery.sizeOf(context).height * 0.2,
+        padding: const EdgeInsets.symmetric(horizontal: 8),
         child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Icon(FontAwesomeIcons.facebook,
-                      color: Colors.white, size: 40),
+                Icon(FontAwesomeIcons.facebook, color: Colors.white, size: 40),
+                SizedBox(
+                  width: 16,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Icon(FontAwesomeIcons.instagram,
-                      color: Colors.white, size: 40),
+                Icon(FontAwesomeIcons.instagram, color: Colors.white, size: 40),
+                SizedBox(
+                  width: 16,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Icon(FontAwesomeIcons.youtube,
-                      color: Colors.white, size: 40),
+                Icon(FontAwesomeIcons.youtube, color: Colors.white, size: 40),
+                SizedBox(
+                  width: 5,
                 ),
               ],
             ),
